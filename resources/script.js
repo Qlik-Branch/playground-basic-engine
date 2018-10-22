422216786377f70b2fd51fdce53203331fea877f
Nick Webster <nick@websy.io>
2018-10-22 18:57:16 +0100

    switch to using enigma logic

--- resources/script.js
+++ resources/script.js
@@ -1,10 +1,26 @@
-var config={};
-Playground.authenticate(config, "qsocks").then(function(ticket){
-  config.ticket = ticket;
-  qsocks.ConnectOpenApp(config).then(function(result){
-    var global = result[0];
-    var app = result[1];
-    console.log(global);
-    console.log(app);
+var config={}
+// Change the schema version below as necessary to match your environment
+getSchema("12.170.2").then(schema => {
+	config.schema = schema
+	return Playground.authenticate(config, "ticket")
+}).then(function(ticket){
+  config.url += `?qlikTicket=${ticket}`
+	const session = enigma.create(config)
+  session.open().then(global => {
+		console.log(global)
+		global.openDoc(config.appname).then(app => {
+			console.log(app)
+		})    
   });
 });
+
+function getSchema(version) {
+	return new Promise((resolve, reject) => {
+	    const xhr = new XMLHttpRequest()
+	    xhr.open("GET", url)
+			xhr.setRequestHeader("Access-Control-Allow-Origin","http://localhost:8000");
+	    xhr.onload = () => resolve(JSON.parse(xhr.responseText))
+	    xhr.onerror = () => reject(xhr.statusText)
+	    xhr.send()
+	  })
+}
